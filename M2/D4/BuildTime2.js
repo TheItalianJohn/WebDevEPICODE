f1d2af86df198581da3478676fcd66ddcfa05d31
Giovanni Norillo <107935348+TheItalianJohn@users.noreply.github.com>
2023-02-19 20:18:59 +0100

    build m2 d4 completata

---
+++ M2/D4/BuildTime2.js
@@ -0,0 +1,47 @@
+// Carrello e sconti particolari - BuildTime #2
+
+/*
+Oggi il tuo compito è creare la logica per un sito di e-commerce che deve supportare sconti extra per utenti speciali.
+A partire da una lista di prezzi, un utente e un costo di spedizione l'algoritmo deve determinare il costo totale del carrello.
+
+Se l'utente ha la proprietà "isAmbassador" con valore true, il carrello deve venire scontato del 30%, PRIMA di calcolare la spedizione (anche gli utenti speciali pagano le spedizioni).
+Se l'utente ha la proprietà "isAmbassador" con valore false, il carrello NON deve venire scontato.
+In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione.
+
+In basso troverai degli esempi di utenti, una lista prezzi e un costo fisso di spedizione.
+*/
+
+
+const marco = {
+  name: 'Marco',
+  lastName: 'Rossi',
+  isAmbassador: true,
+}
+
+const paul = {
+  name: 'Paul',
+  lastName: 'Flynn',
+  isAmbassador: false,
+}
+
+const amy = {
+  name: 'Amy',
+  lastName: 'Reed',
+  isAmbassador: false,
+}
+
+const cart = [34, 5, 2, 100]
+const shippingCost = 10
+let totalCart = 0
+  for (i=0;i<cart.length;i++){
+    totalCart = totalCart + cart[i]
+  }
+if (marco.isAmbassador == true){
+    totalCart = (totalCart-(totalCart*30/100))
+}
+
+if (totalCart < 100){
+    totalCart = totalCart + shippingCost
+}
+
+console.log(totalCart)
